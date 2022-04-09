bd332a345fc000c24c37d5ef31482eeeefa93d4f
MZainKh <khan.zain564@gmail.com>
2022-04-09 16:50:40 +0400

    Dissertation files

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,67 @@
-import { StyleSheet } from 'react-native';
+import * as React from 'react';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    <View style={styles.pageList}>
+      <View style={styles.container}>
+        <Image source={require('../assets/images/gojou.jpeg')} style={styles.image}></Image>
+        <View style={styles.rContainer}>
+          <View style={styles.row}>
+            <Text style={styles.name}>Gojo Satoru</Text>
+            <Text style={styles.text}>04:07 PM</Text>
+          </View>
+          <Text numberOfLines={1} style={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde asperiores iste est praesentium iure, perspiciatis quod ipsum repellat inventore nesciunt!</Text>
+          <View style={styles.unreadContainer}>
+            <Text style={styles.unreadText}>7</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 10
   },
-  title: {
-    fontSize: 20,
+  image: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginRight: 10
+  },
+  rContainer: {
+    flex: 0.9,
+    justifyContent: 'center'
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  name: {
+    fontSize: 18,
     fontWeight: 'bold',
+    color: 'white',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  text: {
+    color: 'grey'
   },
+  unreadContainer: {
+    backgroundColor: 'lightgrey',
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: -30,
+  },
+  unreadText: {
+    fontSize: 10,
+    color: 'black'
+  }
 });
