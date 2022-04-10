eb9ac0f9fb7a5bcfa849964d3fa8f6c0b8e54abb
MZainKh <khan.zain564@gmail.com>
2022-04-10 06:18:04 +0400

    chat item pressed navigation

--- components/ChatItem/ChatItem.tsx
+++ components/ChatItem/ChatItem.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
-import { Asset } from 'expo-asset';
+import { Text, View, Image, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 import styles from './styles';
 
 export default function ChatItem({ chatRoom }) {
     const user = chatRoom.users[1];
+
+    const navigation = useNavigation();
+
+    const onClick = () => {
+        navigation.navigate('Chat', { id: chatRoom.id });
+    }
+
     return (
-      <View style = {styles.container}>
+      <Pressable onPress = {onClick} style = {styles.container}>
           <Image source = {{uri: user.imageUri}} style = {styles.image}></Image>
           <View style={styles.rContainer}>
               <View style = {styles.row}>
@@ -18,7 +25,7 @@ export default function ChatItem({ chatRoom }) {
                   <Text style = {styles.unreadText}>{chatRoom.newMessages}</Text>
               </View> : null}
           </View>
-      </View>
+      </Pressable>
   )
 }
 
